1a6b0cfcaf48481aaed03355b280be91929d33d0
park-seok-hoon <tjrgnsdx1@naver.com>
2023-12-08 17:14:23 +0900

    자바 1일차 : 기본형 변수 선언 예제

---
+++ java_iik/src/day01/VariableEx3.java
@@ -0,0 +1,22 @@
+package day01;
+
+public class VariableEx3 {
+
+	//실수형 변수 선언 예제
+	public static void main(String[] args) {
+    
+	//실수 리터럴 1.23의 타입은 기본 double이다  
+	//1.23뒤에 f를 붙이면 float이 됨.
+	//1.23을 float에 저장하려면 접미사 F를 붙여야 함
+		float num1 = 1.23f;
+		System.out.println(num1);
+		
+		double num2 = 3.14;
+		System.out.println(num2);
+		
+	   
+	
+
+	}
+
+}
---
+++ java_iik/src/day01/VariableEx4.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class VariableEx4 {
+
+	//논리형 변수 선언 예제
+	public static void main(String[] args) {
+		boolean isEven =true;
+		System.out.println(isEven);
+		isEven = 3 % 2 == 0;
+		
+		System.out.println("is Even number?"+isEven);
+		
+		
+	}
+
+}
