9d5eaf67c1955a690524bf12f98ea06505a44102
park-seok-hoon <tjrgnsdx1@naver.com>
2023-12-11 17:37:13 +0900

    자바 2일차 숙제

---
+++ java_iik/src/day02/HomeworkEx1.java
@@ -0,0 +1,43 @@
+package day02;
+
+import java.util.Scanner;
+
+public class HomeworkEx1 {
+
+	public static void main(String[] args) {
+		/* 성적을 입력받아 성적에 맞는 학점을 출력하는 코드를 작성하세요.
+		 * 90이상 ~ 100이하 : A
+		 * 80이상 ~ 90미만 : B
+		 * 70이상 ~ 80미만 : C
+		 * 60이상 ~ 70미만 : D
+		 * 60미만 : F
+		 * 0보다 작거나 100보다 큰 경우 : 잘못된 점수
+		 *
+		 */
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.print("성적을 입력하세요.");
+		int score=sc.nextInt();
+		
+		if(score<0 || score>100)
+			System.out.println("잘못된 점수");
+		else if(score>=90)
+			System.out.println("A");
+		else if(score>=80)
+			System.out.println("B");
+			else if(score>=70)
+				System.out.println("C");
+				else if(score>=60)
+					System.out.println("D");
+				else if(score<60)
+					System.out.println("F");
+						
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
