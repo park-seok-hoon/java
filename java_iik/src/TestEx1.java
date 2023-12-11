93f0a7bf303339f36bf9cada69c09dfd7bb49d11
park-seok-hoon <tjrgnsdx1@naver.com>
2023-12-11 12:46:04 +0900

    자바 2일차 테스트

---
+++ java_iik/src/TestEx1.java
@@ -0,0 +1,17 @@
+
+public class TestEx1 {
+		
+	//연산자 예제
+	public static void main(String[] args) {
+		/* 다음 코드를 이용하여 국어,영어, 수학 성적의 평균을 구하여 콘솔에 출력하는 코드를 작성하세요. */
+      int korscore = 100, engscore = 50, mathscore = 92;
+      
+      System.err.println("평균은 "+ (double)(korscore+engscore+mathscore)/3);
+	  
+      
+      
+      
+	
+	}
+
+}
