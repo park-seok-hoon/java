6b2e881e9011a8925fe3597d9810c41ef3b43f48
park-seok-hoon <tjrgnsdx1@naver.com>
2023-12-12 17:15:42 +0900

    자바 1일차 했던 부분

--- java_iik/src/day01/HelloWorld.java
+++ java_iik/src/day01/HelloWorld.java
@@ -1,10 +1,23 @@
 package day01;
 
+//주석
+/*주석입니다.
+ * 주석입니다.
+ * */
 public class HelloWorld {
 
-	public static void main(String[] args) {
+	
+	/**
+	 * 메소드를 설명할 때 사용하는 주석
+	 * @param args
+	 * 
+	*/
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		//sysout은 콘솔에 출력할 떄 사용
+	System.out.println("Hello World!");
+	System.out.println();
+	
 	}
 		
 	
--- java_iik/src/day01/TypeCastingEx2.java
+++ java_iik/src/day01/TypeCastingEx2.java
@@ -24,6 +24,9 @@ public class TypeCastingEx2 {
 		System.out.println(1/2);
 		System.out.println((double)1/2);
 		
+		//같은 타입의 변수를 여러개 선언하는 방법
+		int num5 = 1 , num6 = 2 ,num7 = 3;
+		
 	}
 
 }
