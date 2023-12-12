bc37adb5201d96ce6e1500c66f8ac0e4a4e8d0e3
park-seok-hoon <tjrgnsdx1@naver.com>
2023-12-12 17:07:34 +0900

    자바 3일차 fo문 예제

---
+++ java_iik/src/day03/ContinuEx1.java
@@ -0,0 +1,28 @@
+package day03;
+
+public class ContinuEx1 {
+
+	// Continue를 이용하여 짝수 출력 예제
+	public static void main(String[] args) {
+		//홀수이면 건너뜀(스킵), 짝수이면 출력
+		
+		for(int i=1; i<=10; i++)
+		{
+			
+			if(i%2==1)
+			continue;
+			System.out.println(i);
+			
+			
+			
+			if(i%2==1)
+				continue;
+	
+			
+		}
+		
+		
+		
+	}
+
+}
