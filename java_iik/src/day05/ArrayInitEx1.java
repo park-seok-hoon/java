2fa687dd4a1ac835cdf6597a371fbe6b5e609ce3
park-seok-hoon <tjrgnsdx1@naver.com>
2023-12-14 17:09:05 +0900

    자바 5일차: 배열 복사와 주소 복사

---
+++ java_iik/src/day05/ArrayInitEx1.java
@@ -0,0 +1,21 @@
+package day05;
+
+public class ArrayInitEx1 {
+
+	public static void main(String[] args) {
+		//arr1배열에는 0번지부터 4번지까지 0으로 초기화가 됨
+		//char는 \0, 정수는 0, 실수는 0.0 boolean는 false
+		
+		int arr1[] =new int[5];
+		
+		//arr2는 0번지에 1, 1번지에 2, 2번지에 3, .... 4번지에 5가 저장
+		//arr2는 총 5개의 크기를 가진 배열이 됨
+		
+		int arr2[] = new int[] {1,2,3,4,5};
+		//arr2와 같음
+		int arr3[]= {1,2,3,4,5};
+		
+		
+	}
+
+}
