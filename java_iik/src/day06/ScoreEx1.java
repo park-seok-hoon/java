04175f221e2cbf1e929ac3fabd0e2c11a325e613
park-seok-hoon <tjrgnsdx1@naver.com>
2023-12-21 17:34:06 +0900

    자바 6일차 혼자 해보기

--- java_iik/src/day06/ScoreEx1.java
+++ java_iik/src/day06/ScoreEx1.java
@@ -1,126 +1,106 @@
 package day06;
+
 import java.util.Scanner;
 public class ScoreEx1 {
-
+	static Scanner sc=new Scanner(System.in);
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		/* 다음 기능을 가진 성적 관리 프로그램을 작성하세요.
-		 * - 1반의 성적을 관리
-		 * - 최대 30명
-		 * - 성적은 번호순으로 관리
-		 * - 성적은 국어, 영어, 수학 성적을 관리
-		 * 메뉴
-		 * 1.성적수정
-		 * 2.성적조회
-		 * 3.프로그램 종료
-		 * 메뉴 선택 : 
-		 * 
-		 * --성적수정--
-		 * 1. 국어
-		 * 2. 영어
-		 * 3. 수학
-		 * 과목 선택 :
-		 * 
-		 * 학생 선택 : (3번학생) 번호를 선택
-		 * 성적 입력 : 100
-		 * 
-		 * --성적조회--
-		 * 1. 과목별조회
-		 * 2. 학생별조회
-		 * 선택 : 1
-		 * 
-		 * --과목별조회--
-		 * 1. 국어
-		 * 2. 영어
-		 * 3. 수학
-		 * 
-		 * 과목 선택 : 
-		 * 1번 : 0점
-		 * 2번 : 0점
-		 * 3번 : 100점
-		 * 
-		 * ...
-		 * 30번 : 0점
-		 * 
-		 * --성적조회--
-		 * 1. 과목별조회
-		 * 2. 학생별조회
-		 * 선택 : 2
-		 * 학생 선택 : 3
-		 * 국어 : 100점
-		 * 영어 : 0점
-		 * 수학 : 0점
-		 * */
-
+		int menu;
+		int subject = 0,studentnum,score,submenu; //과목,학생 번호 선택,성적 입력
+		int maxStudent=30;
+		int [] kor=new int[maxStudent];
+		int [] eng=new int[maxStudent];
+		int [] math=new int[maxStudent]; 
 		
-		 // 메뉴
-		 // 1.성적수정
-		 // 2.성적조회
-		 // 3.프로그램 종료
-		int menu=0;
-		int submenu=0;
-		int select_stud; //학생 선택 변수
-		int subject;    ///과목 변수
-		int score;
-		int Kor[]=new int[30]; //국어 배열 30
-		int Eng[]=new int[30];  //영어 배열 30
-		int Math[]=new int[30]; //수학 배열 30
-		
-	    do
-		{
-			 System.out.print("메뉴\n1.성적수정\n2.성적조회\n3.프로그램 종료");
-			 System.out.print("메뉴 선택:");                              
-			 menu=sc.nextInt();	//메뉴 선택 변수 입력
-		
-			 
-			 switch(menu)
-			 {
-			 case 1:
-			 {  System.out.println("--성적수정--");
-				System.out.println("1. 국어");
-				System.out.println("2. 영어");
-				System.out.println("3. 수학");
-				System.out.print("학생 선택 :");
-				select_stud=sc.nextInt();  	 	//학생 선택 변수 입력
-				System.out.print("과목 선택 :");
-				subject=sc.nextInt(); 			//과목 선택 변수 입력
-				System.out.print("성적 입력 : ");
-				score=sc.nextInt();   			//성적 입력 변수 입력
-			 
-			 }
-			 case 2: //성적조회
-			 {
-				 System.out.println("1. 과목별조회");
-				 System.out.println("2. 학생별조회");
-				 System.out.println("선택 : ");
-				 submenu=sc.nextInt();
-				 
-				 if(submenu==1)
-				 {
-					 System.out.println("1. 국어");
-					 System.out.println("2. 영어");
-					 System.out.println("3. 수학");
-					 System.out.println("과목 선택 : ");
-					 System.out.println("1번 : "+ +"점");
-					 System.out.println("2번 : "+ +"점");
-					 System.out.println("3번 : "+ +"점");
-				 }
-				 else if(submenu==2)
-				 {
-					 
-				 }
-				 
-			 } 
-			 
-			 }
-		
-			 }while(menu==3);
-		
-		
-		
-		
+					do {
+					System.out.println("메뉴");
+					System.out.println("1.성적수정");
+					System.out.println("2.성적조회");
+					System.out.println("3.프로그램 종료");
+					System.out.print
+					("메뉴 선택 :");
+					menu=sc.nextInt();
+					
+					switch(menu)
+					{	
+						case 1: //성적수정 번호
+						System.out.println("1.국어");
+						System.out.println("2.영어");	
+						System.out.println("3.수학");
+						
+						System.out.print("과목 선택:");
+						subject=sc.nextInt();
+						System.out.print("학생 선택");
+						studentnum=sc.nextInt();
+						System.out.print("성적 입력");
+						score=sc.nextInt();
+						
+								switch(subject) //국어 영어 수학 성적 번호별 입력
+								{
+									case 1:
+										kor[studentnum]=score;
+										break;
+									case 2:
+										eng[studentnum]=score;
+										break;
+									case 3:
+										math[studentnum]=score;
+										break;
+								}
+						break;
+						
+						case 2: //성적조회 번호
+							System.out.println("1.과목별조회");
+							System.out.println("2.학생별조회");
+							System.out.print("선택 :");
+							submenu=sc.nextInt();	//성적조회->1,2번 중 택 서브 메뉴
+								
+							switch(submenu)
+								{	
+									case 1: //1.과목별조회
+									
+									System.out.println("1.국어");
+									System.out.println("2.영어");
+									System.out.println("3.수학");
+									System.out.println("과목 선택 :");
+									
+											switch(subject)
+											{
+												case 1:
+													for(int i=0; i<30; i++)
+															System.out.println((i+1)+"번 : "+kor[i]+"점");
+													break;
+												case 2:
+													for(int i=0; i<30; i++)
+															System.out.println((i+1)+"번 : "+eng[i]+"점");
+													break;
+												case 3:
+													for(int i=0; i<30; i++)
+															System.out.println((i+1)+"번 : "+math[i]+"점");
+													break;
+											
+											}
+											break;
+									case 2:  //학생별조회
+									System.out.println("학생 선택 :");
+									studentnum=sc.nextInt();
+									System.out.println("국어 : "+kor[studentnum]);
+									System.out.println("영어 : "+eng[studentnum]);
+									System.out.println("수학 : "+math[studentnum]);
+					
+								}
+							
+							break;
+						case 3:
+							System.out.println("프로그램을 종료합니다.");
+							break;
+						default :
+					 System.out.println("메뉴를 다시 고르세요");
+					}
+					
+					
+					}while(menu!=3);
 	
-	
+		sc.close();
 	}
 
 }
