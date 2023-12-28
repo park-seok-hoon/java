ffa3d1951f0fb04ea92b8eec8bacbae97fa0d912
park-seok-hoon <tjrgnsdx1@naver.com>
2023-12-28 17:28:20 +0900

    자바 14일차 로또 리스트 활용

--- java_iik/src/day14/LottoEx1.java
+++ java_iik/src/day14/LottoEx1.java
@@ -9,53 +9,67 @@ import java.util.Random;
 import java.util.Set;
 
 public class LottoEx1 {
-	public static int count=0;
-	public static int bonusnumber=0;
-	public static void main(String args[] )
-	{   Scanner sc=new Scanner(System.in);
+	public static void main(String[] args) {
 		/* 랜덤으로 당첨 번호를 생성하고, 사용자가 로또 번호를 입력하면 등수를 알려주는 코드를 작성하세요.
-		 * 단 컬렉션을 이용
+		 * 단, 컬렉션을 이용
 		 * 당첨번호 : 6개, 보너스 : 1개
 		 * 사용자 : 6개
-		 * 번호 범위 : 1~45 
-		 * */
-		
-		List<Integer> list =new ArrayList<Integer>();
+		 * 번호 범위 : 1~45
+		 */
+		Set<Integer> lotto = new HashSet<Integer>();
+		Set<Integer> user = new HashSet<Integer>();
+		int bonus;
 		int min = 1, max = 45;
-
+		
+		//로또 당첨번호 6개를 랜덤으로 생성(중복 X)
+		while(lotto.size() < 7) {
+			int tmp = (int)(Math.random() * (max - min + 1) + min);
+			lotto.add(tmp);
+		}
+		//보너스 번호를 생성
+		List<Integer> lotto2 = new ArrayList<Integer>();
+		lotto2.addAll(lotto);
+		bonus = lotto2.remove(lotto2.size() - 1); //인덱스 6에 있는 숫자를 lotto2 리스트에서 삭제하고 리턴되는데 그 값을 bonus 변수에 저장시킴
+		
+		System.out.println(lotto2 + ", 보너스 : " + bonus);
+		
+		//사용자가 당첨 번호를 입력
+				Scanner scan = new Scanner(System.in);
+				System.out.print("번호(중복되지 않은 6개) : ");
+				while(user.size() < 6) {
+					int tmp = scan.nextInt();
+					user.add(tmp);
+				}
+				
+				//일치하는 개수 계산
+				int count = 0;
+				for(int tmp: user) {
+					if(lotto2.contains(tmp)) {
+						count++;
+					}
+				}
+				//등수를 출력
+				switch(count) {
+				case 6:
+					System.out.println("1등입니다.");
+					break;
+				case 5:
+					int rank = user.contains(bonus) ? 2 : 3;
+					System.out.println(rank+"등입니다.");
+					break;
+				case 4:
+					System.out.println("4등입니다.");
+					break;
+				case 3:
+					System.out.println("5등입니다.");
+					break;
+				default:
+					System.out.println("꽝입니다.");
+				}
+				
+		
+		
 	
-		for(int i=0; i<7; i++)
-		{	
-			Random random = new Random();
-			int tmp = random.nextInt(max - min + 1)+ min;
-			list.add(tmp);
-		}
-		
-		for(int i=0; i<list.size(); i++)
-		{
-			System.out.print(list.get(i)+" ");
-		}
-		
-		List<Integer> user = new ArrayList<Integer>();
-		
-		System.out.println();
-		for(int i=0; i<7; i++)
-		{	int number=sc.nextInt();
-			user.add(number);
-		}
-		
-		for(int i=0; i<list.size(); i++)
-		{
-			
-			System.out.print(user.get(i)+" ");
-		}
-		
-		
-		
-	
-	}
-		
-		
-	
+}
 }
 
