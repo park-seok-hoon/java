299bf28141191da98978d9c753f12473b10aba4e
park-seok-hoon <tjrgnsdx1@naver.com>
2024-01-09 15:26:02 +0900

    homework

---
+++ java_iik/src/homework/inoutmainprogram.java
@@ -0,0 +1,15 @@
+package homework;
+
+
+public class inoutmainprogram {		//수입,지출 실행 프로그램
+
+	public static void main(String[] args) {
+		
+		 inoutmanagement iomanage=new inoutmanagement();
+		 
+		 iomanage.run();
+		 
+
+	}
+
+}
